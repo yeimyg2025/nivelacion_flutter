41df1f2c68b4fe2d2b4f8a5f8050db3ee9f95ffb
yeimy <yeimyg833@gmail.com>
2025-08-05 19:12:35 -0400

    Mi primer commit

---
+++ android/app/src/main/kotlin/com/example/nivelacion_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nivelacion_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+//metodo con el que trabajamos
+
+void main(){
+  runApp(MyApp());//contenedor
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
